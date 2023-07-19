26ca8d25c43e27fe8ea3ce4aeb75052fa0047cec
Hakan <70763685+yuuIind@users.noreply.github.com>
2023-07-19 13:59:28 +0300

    Create resistor_color_duo.py

---
+++ Exercism/List Methods/resistor_color_duo.py
@@ -0,0 +1,16 @@
+COLOR = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9,
+}
+
+def value(colors):
+    val = COLOR[colors[0].lower()]*10 + COLOR[colors[1].lower()]
+    return val
